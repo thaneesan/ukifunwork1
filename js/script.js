519d2ca1309323b57e122a9ee1a17b7c722b29b7
ukistu11 <sthaneesan@gamil.com>
2017-07-04 14:21:57 +0530

    my_first_fun_work

---
+++ js/script.js
@@ -0,0 +1,78 @@
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {
+    SC.stream('/tracks/168179704',function(sound){
+         $('#start1').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop1').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/163423901',function(sound){
+         $('#start2').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop2').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/66071178',function(sound){
+         $('#start3').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop3').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/173263027',function(sound){
+         $('#start4').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop4').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/215980258',function(sound){
+         $('#start5').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop5').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+    SC.stream('/tracks/298',function(sound){
+         $('#start6').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+         $('#stop6').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+    });
+
+});
